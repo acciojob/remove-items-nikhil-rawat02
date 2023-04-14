4f88cbe815ec2eadead91c215437d4637a7fc148
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-14 23:36:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+let buttons = document.querySelectorAll('input[type=button]');
+let btn = buttons[0];
+let selec = document.getElementById("colorSelect");
+let arr = selec.options;
+
+function func (){
+  let val = selec.options[selec.selectedIndex].text;
+    for(let element of arr){
+        console.log(element.innerHTML);
+        if(element.innerHTML === val){
+
+        }   
+    }
+}
+
+btn.addEventListener("click", func);
